67c9401a186d58dbd1849e1a8c12b77eacfb16c7
bmwasoga <bmwasonga@gmail.com>
2021-04-04 15:47:57 +0300

    the fetch is working perfectly

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,13 +1,13 @@
 import React, {useState , useEffect} from 'react'
 import axios from 'axios'
 //attempt to import the styles
+import './styles.js/styles.css'
 
 
 
 //the below is information that is needed to full data from the movie db API
 const api_url =
   'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6035714427d8af477272a1cfd680cfd6';
-const image_path = 'https://image.tmdb.org/t/p/w1280';
 const search_url =
   'https://api.themoviedb.org/3/search/movie?api_key=6035714427d8af477272a1cfd680cfd6&query="';
 
@@ -24,23 +24,30 @@ const search_url =
         .then(res => {
           setMovies(res.data.results);
         })
-        .catch((error) => console.log(error, "Error , please fix!"));
+        .catch((error) => console.log(error, "error"));
     }, []);
   
     return (
-      <div className="movie_container">
+      <div className="movie-container">
         
-        {movies.map((movie, i) => {
+        {movies.map((movie, idx) => {
+        const image_path = 'https://image.tmdb.org/t/p/w1280';
+          
           return (
-            <h2>
-              <img
-                src={image_path + movie.poster_path}
-                alt="This is a movie"
-                className="movie_here"
-                key={i}
-              />
-              {movie.title}
-            </h2>
+            < > 
+            <div className='movie' id={idx}> 
+             <img src={image_path +movie.poster_path} alt={movie.title}/>
+             <div className="movie-info">
+               <h3  > {movie.title}</h3>
+               <span className='voteaverage' > {movie.vote_average}</span>
+
+             </div>
+             <div className='overview' >
+               <h3 >{movie.overview}</h3>
+             </div>
+
+            </div> 
+            </>
           );
         })}
       </div>
